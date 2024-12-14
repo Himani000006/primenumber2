d1668a2d409eae0e7fbc3ccc16e66a0ad8dccbfe
himani <ginnelahimanigmail.com>
2024-12-14 21:31:03 +0530

    primenumber

---
+++ primenumber.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class primenumber {
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false; // Numbers less than or equal to 1 are not prime
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false; // If divisible by any number other than 1 and itself, not prime
+            }
+        }
+        return true;
+    }
+
+    // Method to print all prime numbers within a given range
+    public static void printPrimesInRange(int start, int end) {
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Check if a single number is prime
+        System.out.print("Enter a number to check if it is prime: ");
+        int number = scanner.nextInt();
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        // Print prime numbers in a range
+        System.out.print("Enter the start of the range: ");
+        int start = scanner.nextInt();
+        System.out.print("Enter the end of the range: ");
+        int end = scanner.nextInt();
+
+        printPrimesInRange(start, end);
+
+        scanner.close();
+    }
+}
+
